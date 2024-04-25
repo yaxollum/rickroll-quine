063b9bedf622d3b4170e411ab77d1b9cf85e6f46
Peter Ye <yaxollum@gmail.com>
2024-04-24 23:15:25 -0400

    even closer ...

--- generate.py
+++ generate.py
@@ -5,7 +5,18 @@ import string
 import sys
 
 
+def join_lists(lists):
+    return list(itertools.chain.from_iterable(lists))
+
+
+# track variable declarations
+_vars = set()
+
+
 def declare_var(name):
+    if name in _vars:
+        raise NameError(f"variable '{name}' declared multiple times")
+    _vars.add(name)
     return [f"Never gonna let {name} down"]
 
 
@@ -17,6 +28,10 @@ def define_var(name, val):
     return declare_var(name) + set_var(name, val)
 
 
+def increment_var(name):
+    return set_var(name, f"{name} + 1")
+
+
 def call_fn(fn, args, rtn):
     args_str = ", ".join(args) if len(args) > 0 else "you"
     return [f"(Ooh give you {rtn}) Never gonna run {fn} and desert {args_str}"]
@@ -25,6 +40,14 @@ def call_fn(fn, args, rtn):
 def call_fn_no_rtn(fn, args):
     assert len(args) > 0
     return [f"Never gonna run {fn} and desert {', '.join(args)}"]
+
+
+def put_char(name):
+    return call_fn_no_rtn(PUT_CHAR, [name])
+
+
+def put_string(s):
+    return join_lists(put_char(letter_var(c)) for c in s)
 
 
 def array_index(array, index):
@@ -39,6 +62,10 @@ def while_loop(cond, body):
     )
 
 
+def less_than(val1, val2):
+    return f"{val1} < {val2}"
+
+
 SPECIAL_CHARS = {
     " ": "space",
     "\n": "newline",
@@ -51,9 +78,17 @@ SPECIAL_CHARS = {
     "<": "lt",
     ":": "colon",
     "+": "plus",
-    "_": "underscore",
+    ",": "comma",
     "0": "zero",
     "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
 }
 
 
@@ -83,40 +118,60 @@ def define_letter(letter):
 
 chars = string.ascii_letters + "".join(SPECIAL_CHARS)
 
-define_chars = list(itertools.chain.from_iterable(map(define_letter, chars)))
+define_chars = join_lists(map(define_letter, chars))
 
 SRC_VAR = "src"
 ARRAY_OF = "ArrayOf"
-ARRAY_PUSH = "ArrayPush"
 ARRAY_LENGTH = "ArrayLength"
 PUT_CHAR = "PutChar"
-SRC_LEN_VAR = "src_len"
+SRC_LEN_VAR = "srclen"
 COUNTER_VAR = "counter"
 TMP_VAR = "tmp"
 
 
 def lines_to_str(lines):
-    return "\n".join(lines)
+    return "\n".join(lines) + "\n"
 
 
-program = ["[Chorus]"] + define_chars + declare_var(SRC_VAR)
-
-loop_body = (
-    define_var(TMP_VAR, array_index(SRC_VAR, COUNTER_VAR))
-    + call_fn_no_rtn(PUT_CHAR, [TMP_VAR])
-    + set_var(COUNTER_VAR, f"{COUNTER_VAR} + 1")
+prog_part1 = (
+    ["[Chorus]"]
+    + define_chars
+    + declare_var(SRC_VAR)
+    + declare_var(TMP_VAR)
+    + declare_var(COUNTER_VAR)
+    + declare_var(SRC_LEN_VAR)
 )
 
-print_src = (
-    declare_var(SRC_LEN_VAR)
+
+loop_body1 = (
+    set_var(TMP_VAR, array_index(SRC_VAR, COUNTER_VAR))
+    + put_char(TMP_VAR)
+    + increment_var(COUNTER_VAR)
+)
+
+loop_body2 = (
+    set_var(TMP_VAR, array_index(SRC_VAR, COUNTER_VAR))
+    + put_char(TMP_VAR)
+    + increment_var(COUNTER_VAR)
+)
+
+magic_num = len(lines_to_str(prog_part1))
+prog_part2 = (
+    # print prog_part1:
+    set_var(COUNTER_VAR, 0)
+    + while_loop(less_than(COUNTER_VAR, magic_num), loop_body1)
+    # print build_src:
+    + put_string(f"(Ooh give you {SRC_VAR}) Never gonna run {ARRAY_OF} and desert ")
+    + set_var(COUNTER_VAR, 0)
     + call_fn(ARRAY_LENGTH, [SRC_VAR], SRC_LEN_VAR)
-    + define_var(COUNTER_VAR, 0)
-    + while_loop(f"{COUNTER_VAR} < {SRC_LEN_VAR}", loop_body)
+    + while_loop(less_than(COUNTER_VAR, SRC_LEN_VAR), loop_body2)
 )
 
 build_src = call_fn(
-    ARRAY_OF, list(map(letter_var, lines_to_str(program + print_src) + "\n")), SRC_VAR
+    ARRAY_OF,
+    list(map(letter_var, lines_to_str(prog_part1 + prog_part2))),
+    SRC_VAR,
 )
 
 
-print(lines_to_str(program + build_src + print_src))
+print(lines_to_str(prog_part1 + build_src + prog_part2), end="")
